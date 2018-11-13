3d56dad93856f550f9ba740ebe53ca2e74c0abdd
Dimitri Lozeve <dimitri@lozeve.com>
2018-11-13 21:16:39 +0100

    Add posts

--- site.hs
+++ site.hs
@@ -11,7 +11,7 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-  match "images/*" $ do
+  match "images/**" $ do
     route   idRoute
     compile copyFileCompiler
 
